995a9c2eb0dd91ef842b3b89731a28071a6e8784
hos1miya <113900368+hos1miya@users.noreply.github.com>
2025-02-03 19:44:37 +0900

    dice fix

--- src/modules/dice/index.ts
+++ src/modules/dice/index.ts
@@ -33,7 +33,7 @@ export default class extends Module {
 			results.push(Math.floor(Math.random() * dice) + 1);
 		}
 
-		const message = msg.text.replace(/\d[dD]\d/g, results.join(" "))
+		const message = msg.text.replace(/\d+[dD]\d+/g, results.join(" "))
 		msg.reply(serifs.dice.done(message));
 
 		return true;
